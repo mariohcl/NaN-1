32b42f53305fefaaba72c0f6f9615467707ea270
mario <air.hammer@gmail.com>
2020-01-14 21:58:11 -0300

    Agregados metodos de proyectos y usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post '/projects/save_users' => 'projects#save_users', as: 'save_users'
+
+  root to: "projects#index"
 end
